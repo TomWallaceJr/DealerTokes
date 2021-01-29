be697cf21fb91a30c60c57129b7d1c8aa0d278b6
twallace1015@gmail.com <68206140+TomWallaceJr@users.noreply.github.com>
2021-01-29 01:55:42 -0500

    write submitForm func in DateDetails Component... POST request getting denied 400 (Bad Request)

--- src/components/DateDetails/DateDetails.js
+++ src/components/DateDetails/DateDetails.js
@@ -2,19 +2,51 @@ import React, { Component } from 'react'
 import { Input, Label, Textarea } from '../Form/Form'
 import Button from '../Button/Button'
 import UserContext from '../../contexts/UserContext'
+import WorkdayApiService from '../../services/workday-api-service'
 
 export default class DateDetails extends Component {
     // maybe default props here
-    // static defaultProps = {
-    // newWorkdaySubmitSuccess: () => { }
-    // }
+    static defaultProps = {
+        newWorkdaySubmitSuccess: () => { }
+    }
 
     static contextType = UserContext
     state = { error: null }
 
+
     handleSubmit = e => {
-        // code when form submitted
-        console.log('OK')
+        e.preventDefault();
+        console.log(this.context.user.id)
+        // get correct date format
+        // function getCurrentDateTimeMySql() {
+        //     var tzoffset = (new Date()).getTimezoneOffset() * 60000; //offset in milliseconds
+        //     var localISOTime = (new Date(Date.now() - tzoffset)).toISOString().slice(0, 19).replace('T', ' ');
+        //     var mySqlDT = localISOTime;
+        //     return mySqlDT;
+        // }
+        // let date = getCurrentDateTimeMySql()
+
+        const { date, hours, downs, tokes, notes } = e.target;
+        WorkdayApiService.postNewWorkday({
+            date: this.props.date,
+            hours: hours.value,
+            downs: downs.value,
+            tokes: tokes.value,
+            notes: notes.value,
+            user_id: this.context.user.id
+        })
+            .then(workday => {
+                console.log('inside .then')
+                date.value = ''
+                hours.value = ''
+                downs.value = ''
+                tokes.value = ''
+                notes.value = ''
+                this.props.newWorkdaySubmitSuccess()
+            })
+            .catch(res => {
+                this.setState({ error: res.error })
+            })
     }
 
     render() {
@@ -24,47 +56,48 @@ export default class DateDetails extends Component {
                 className='new-workday-form'
                 onSubmit={this.handleSubmit}
             >
-                <div role='alert'>
+                {/* <div role='alert'>
                     {error && <p>{error}</p>}
-                </div>
+                </div> */}
                 <div>
                     <Label htmlFor='date-input'>Date</Label>
                     <Input
-                        value={this.props.date}
-                        id='date-input'
-                        name='date-input'
+                        defaultValue={this.props.date}
+                        readOnly
+                        id='date'
+                        name='date'
                         required
                     />
                 </div>
                 <div>
-                    <Label htmlFor='hours-input'>Hours Worked</Label>
+                    <Label htmlFor='hours'>Hours Worked</Label>
                     <Input
-                        id='hours-input'
-                        name='hours-input'
+                        id='hours'
+                        name='hours'
                         required
                     />
                 </div>
                 <div>
-                    <Label htmlFor='downs-input'>Downs Dealt</Label>
+                    <Label htmlFor='downs'>Downs Dealt</Label>
                     <Input
-                        id='downs-input'
-                        name='downs-input'
+                        id='downs'
+                        name='downs'
                         required
                     />
                 </div>
                 <div>
-                    <Label htmlFor='tokes-input'>Tokes Earned</Label>
+                    <Label htmlFor='tokes'>Tokes Earned</Label>
                     <Input
-                        id='tokes-input'
-                        name='tokes-input'
+                        id='tokes'
+                        name='tokes'
                         required
                     />
                 </div>
                 <div>
-                    <Label htmlFor='notes-input'>Notes</Label>
+                    <Label htmlFor='notes'>Notes</Label>
                     <Textarea
-                        id='notes-input'
-                        name='notes-input'
+                        id='notes'
+                        name='notes'
                     />
                 </div>
                 <Button type='submit'>Add New Workday</Button>
--- src/routes/CalendarRoute/CalendarRoute.js
+++ src/routes/CalendarRoute/CalendarRoute.js
@@ -1,12 +1,14 @@
 import React, { Component } from "react";
 import TokenService from "../../services/token-service";
 import UserContext from "../../contexts/UserContext";
+import WorkdayApiService from '../../services/workday-api-service';
 import { Link } from 'react-router-dom';
 import config from "../../config";
 import Calendar from 'react-calendar';
 import 'react-calendar/dist/Calendar.css';
-import './CalendarRoute.css'
+import './CalendarRoute.css';
 import DateDetails from "../../components/DateDetails/DateDetails";
+
 
 
 
