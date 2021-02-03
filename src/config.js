58f264ec468047a1d06a8cd7de483cb9900f6fa3
twallace1015@gmail.com <68206140+TomWallaceJr@users.noreply.github.com>
2021-02-03 03:10:31 -0500

    README updated

--- src/config.js
+++ src/config.js
@@ -1,6 +1,7 @@
 export default {
-  API_ENDPOINT: 'http://localhost:8000/api',
+  API_ENDPOINT: 'https://pacific-retreat-10215.herokuapp.com/api',
 }
 
 
-
+// https://pacific-retreat-10215.herokuapp.com/api
+// http://localhost:8000/api
